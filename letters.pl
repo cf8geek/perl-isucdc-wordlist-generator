c3ff56759094333d543db474abb9fa60eea27b35
cf8geek <cf8geek@users.noreply.github.com>
2015-04-26 21:52:40 -0500

    removed extra tabs

--- letters.pl
+++ letters.pl
@@ -2,6 +2,7 @@
 
 use warnings;
 use strict;
+
 our $c = 0; # counter for combined set (full password)
 our $h; # counter for letter set, total
 our $i; # counter for letter set, first letter
@@ -14,15 +15,15 @@ our @letter_set; # array to hold the letter sets (consonant, vowel, consonant)
 
 &letters; # makes the letter sets
 sub letters() {
-		$h = 0;
-		for ($i = 0; $i < 21; $i++) { # first letter (consonant)
-				for ($j = 0; $j < 5; $j++) { # second letter (vowel)
-						for ($k = 0; $k < 21; $k++) { # third letter (consonant)
-								$letter_set[$h] = $var_con[$i] . $var_vow[$j] . $var_con[$k];
-								print $letter_set[$h]."\n";
-								$h++;								
-						}
-				}
+	$h = 0;
+	for ($i = 0; $i < 21; $i++) { # first letter (consonant)
+		for ($j = 0; $j < 5; $j++) { # second letter (vowel)
+			for ($k = 0; $k < 21; $k++) { # third letter (consonant)
+				$letter_set[$h] = $var_con[$i] . $var_vow[$j] . $var_con[$k];
+				print $letter_set[$h]."\n";
+				$h++;								
+			}
 		}
+	}
 }
 #EOF
